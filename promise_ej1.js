7729bf84d36901e66b4651483b29be9408e315ca
christianbrusa <christianbrusa@hotmail.com>
2022-06-20 20:01:26 -0300

    Creo variable y le asigno la función

--- promise_ej1.js
+++ promise_ej1.js
@@ -1,11 +1,11 @@
 const request = require("request-promise");
 
-function ejercicio1 (item){
+const ejercicio1 = (item) =>{
     let options = {
         url: `https://api.mercadolibre.com/items/${item}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-061809-1d73012437e4adaf13dc5068d2564a2e-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-062017-2c6b129525f4964a9a71977d73059c97-138427624"
         },
         json: true
     }
@@ -15,6 +15,3 @@ function ejercicio1 (item){
 ejercicio1("MLA1132828285").then(respuesta => {
     console.log("El sellerId es: " + respuesta.seller_id)
 })
-
-
-
