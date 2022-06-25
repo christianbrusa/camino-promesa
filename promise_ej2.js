9259ff03a155f0be8a2eb0c87dfa0de78ca7577f
christianbrusa <christianbrusa@hotmail.com>
2022-06-25 19:41:03 -0300

    Refactor separando tareas por función
    La idea de esto es que el codigo sea mantenible

--- promise_ej2.js
+++ promise_ej2.js
@@ -1,24 +1,29 @@
 const request = require("request-promise");
 
-const obtenerSellerIdConItemId = (item) =>{
+const obtenerItem = (item) => {
     let options = {
         url: `https://api.mercadolibre.com/items/${item}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-062017-2c6b129525f4964a9a71977d73059c97-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-062516-cc3eb705610c6b4618655b8902e0547f-138427624"
         },
         json: true
     }
     return request(options);
 }
 
-obtenerSellerIdConItemId("MLA1132828285").then(respuesta => {
-    console.log({
-        "idVendedor": respuesta.seller_id,
-        "idItem":respuesta.id
-        /*"idItem2": obtenerSellerIdConItemId("MLA1132828285")._rp_options.url.split("/")[4]*/
-    })
-})
 
+const obtenerSellerIdConItemId = (item) => {
 
+    obtenerItem(item).then(respuesta => {
+        return {
+            idVendedor: respuesta.seller_id,
+            idItem: respuesta.id
+        }
+    }).tap(resultado => console.log({
+        "idVendedor": resultado.idVendedor,
+        "idItem": resultado.idItem
+    }))
+}
 
+obtenerSellerIdConItemId("MLA1132828285");
