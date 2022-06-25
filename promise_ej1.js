25cf24a2d665d1973bcbac3c96b9cc11b44399c0
christianbrusa <christianbrusa@hotmail.com>
2022-06-25 01:56:27 -0300

    Refactor separando tareas por función
    La idea de esto es que el codigo sea mantenible

--- promise_ej1.js
+++ promise_ej1.js
@@ -1,17 +1,23 @@
 const request = require("request-promise");
 
-const obtenerSellerId = (item) =>{
+const obtenerItem = (item) => {
     let options = {
         url: `https://api.mercadolibre.com/items/${item}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-062017-2c6b129525f4964a9a71977d73059c97-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-062421-19af77531e88f5201e098cb3abeb11dd-138427624"
         },
         json: true
     }
     return request(options);
 }
 
-obtenerSellerId("MLA1132828285").then(respuesta => {
-    console.log("El sellerId es: " + respuesta.seller_id)
-})
+const obtenerSellerId = (item) => {
+    obtenerItem(item).then(respuesta => {
+        return {
+            sellerId: respuesta.seller_id
+        }
+    }).tap(resultado => console.log(resultado.sellerId))
+}
+
+obtenerSellerId("MLA1132828285");
