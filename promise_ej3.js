289a6f2f749f4491ffcaf214450f93ad5a93a8a6
christianbrusa <christianbrusa@hotmail.com>
2022-07-08 22:02:12 -0300

    Renombro función

--- promise_ej3.js
+++ promise_ej3.js
@@ -1,19 +1,19 @@
 const request = require("request-promise");
 
-const obtenerSellerNickname = (sellerId) =>{
-	let options = {
-		url: `https://api.mercadolibre.com/users/${sellerId}`,
+const obtenerSeller = (sellerId) =>{
+    let options = {
+        url: `https://api.mercadolibre.com/users/${sellerId}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-062017-2c6b129525f4964a9a71977d73059c97-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-070816-25a4de716b0b47ea00da9e0acbfde96c-138427624"
         },
         json: true
-	}
-	return request(options);
+    }
+    return request(options);
 }
 
 const imprimirSellerNickname = (sellerId) => obtenerSellerNickname(sellerId).then(respuesta => {
-	console.log("el nickname del user es: "+ respuesta.nickname);
+    console.log("el nickname del user es: "+ respuesta.nickname);
 })
 
 imprimirSellerNickname(138427624);
