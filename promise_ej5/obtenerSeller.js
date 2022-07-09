f241876dededc58107b40ccbfa503562014d74da
christianbrusa <christianbrusa@hotmail.com>
2022-07-09 19:32:34 -0300

    Refactor

--- promise_ej5/obtenerSeller.js
+++ promise_ej5/obtenerSeller.js
@@ -5,7 +5,7 @@ const obtenerSeller = (sellerId) => {
         url: `https://api.mercadolibre.com/users/${sellerId}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-063015-d31c3b675cc9369b195af8a057f7a65d-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-070916-86513ef013cb9fcec5c8cb3508c9d9eb-138427624"
         },
         json: true
     }
--- promise_ej5/promise_ej5.js
+++ promise_ej5/promise_ej5.js
@@ -2,14 +2,15 @@ const obtenerItem = require("./obtenerItem");
 const obtenerSeller = require("./obtenerSeller");
 const request = require("request-promise");
 
+const obtenerSellerDeItem = (itemId) => {
+    return obtenerItem(itemId)
+    .then(item => obtenerSeller(item.seller_id))
+}
+
 const obtenerSellerNickname = (itemId) => {
-    obtenerItem(itemId).then(item => {
-        obtenerSeller(item.seller_id).then(seller => {
-            return {
-                SellerNickname: seller.nickname
-            }
-        }).tap(resultado => console.log(resultado.SellerNickname));
-    })
+    return obtenerSellerDeItem(itemId)
+    .then(seller => seller.nickname)
+    .tap(nickname => console.log(nickname))
 }
 
 obtenerSellerNickname("MLA1132828285");
