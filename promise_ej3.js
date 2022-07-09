80169a7a90f64159979b49f3ab895435ceafac64
christianbrusa <christianbrusa@hotmail.com>
2022-07-08 23:15:28 -0300

    Refactor función intermedia

--- promise_ej3.js
+++ promise_ej3.js
@@ -5,15 +5,21 @@ const obtenerSeller = (sellerId) =>{
         url: `https://api.mercadolibre.com/users/${sellerId}`,
         method: "GET",
         headers: {
-            Authorization: "Bearer APP_USR-2010260036191127-070816-25a4de716b0b47ea00da9e0acbfde96c-138427624"
+            Authorization: "Bearer APP_USR-2010260036191127-070822-89376ce593ee1e880166060d4eafffba-138427624"
         },
         json: true
     }
     return request(options);
 }
 
-const imprimirSellerNickname = (sellerId) => obtenerSellerNickname(sellerId).then(respuesta => {
-    console.log("el nickname del user es: "+ respuesta.nickname);
+const obtenerSellerNickname = (sellerId) => {
+    obtenerSeller(sellerId).then(seller => {
+        return seller.nickname
+    })
+}
+
+const imprimirSellerNickname = (sellerId) => obtenerSellerNickname(sellerId).then(nickname => {
+    console.log("el nickname del user es: " + nickname);
 })
 
 imprimirSellerNickname(138427624);
